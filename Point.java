32bdaaac16a42faf89ba8e27a5aab24863bb5e96
Jason Zhang <jzhang20@stuy.edu>
2020-10-29 21:05:11 -0400

    distanceTo complete

---
+++ Point.java
@@ -0,0 +1,27 @@
+public class Point {
+  private double x, y;
+
+  public Point (double X, double Y) {
+    x = X;
+    y = Y;
+  }
+
+  public Point (Point p) {
+    x = p.x;
+    y = p.y;
+  }
+
+  public double getX(){
+    return x;
+  }
+
+  public double getY(){
+    return y;
+  }
+
+  public double distanceTo (Point other) {
+    double a = other.getX();
+    double b = other.getY();
+    return Math.sqrt((x - a) * (x - a) + (y - b) * (y - b));
+  }
+}
---
+++ Tester.java
@@ -0,0 +1,7 @@
+public class Tester {
+  public static void main (String[] args) {
+    Point a = new Point (0, 0);
+    Point b = new Point (3, 4);
+    System.out.println(a.distanceTo(b));
+  }
+}
