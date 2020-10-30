9737aea505ee453aea6a94c85ee00fb06fca6672
Jason Zhang <jzhang20@stuy.edu>
2020-10-30 00:26:25 -0400

    getArea and classify tested

--- Tester.java
+++ Tester.java
@@ -4,10 +4,29 @@ public class Tester {
     Point b = new Point (3, 4);
     Point c = new Point (0, 8);
     Point d = new Point (0, 0);
-    Triangle x = new Triangle (a, b, c);
+    Triangle w = new Triangle (a, b, c);
+    Triangle x = new Triangle (0, 0, 3, 4, 0, 8);
+    System.out.println("DISTANCE");
     System.out.println(a.distanceTo(b));
+    System.out.println("POINT EQUALS");
     System.out.println(a.equals(b));
     System.out.println(a.equals(d));
+    System.out.println("PERIMETERS");
+    System.out.println(w.getPerimeter());
     System.out.println(x.getPerimeter());
+    System.out.println("AREA");
+    System.out.println(w.getArea());
+    System.out.println(x.getArea());
+    System.out.println("CLASSIFY (THEORETICALLY CONGRUENT TRIANGLE)");
+    System.out.println(w.classify());
+    System.out.println(x.classify());
+    System.out.println("CLASSIFY (GENERAL TRIANGLES)");
+    Point g = new Point (0, 0);
+    Point h = new Point (3, 4);
+    Point i = new Point (0, 6);
+    Triangle y = new Triangle (0, 0, 2.5, 2.5 * Math.sqrt (3), 5, 0);
+    Triangle z = new Triangle (g, h, i);
+    System.out.println(y.classify());
+    System.out.println(z.classify());
   }
 }
--- Triangle.java
+++ Triangle.java
@@ -19,4 +19,35 @@ public class Triangle {
     double d3 = v2.distanceTo(v3);
     return d1 + d2 + d3;
   }
+
+  public double getArea () {
+    double d1 = v1.distanceTo(v2);
+    double d2 = v1.distanceTo(v3);
+    double d3 = v2.distanceTo(v3);
+    double s = (d1 + d2 + d3) / 2;
+    double a = Math.sqrt (s * (s - d1) * (s - d2) * (s - d3));
+    return a;
+  }
+
+  public String classify () {
+    double d1 = (Math.round(v1.distanceTo(v2) * 10000) / 10000.0);
+    double d2 = (Math.round(v1.distanceTo(v3) * 10000) / 10000.0);
+    double d3 = (Math.round(v2.distanceTo(v3) * 10000) / 10000.0);
+    if (d1 == d2) {
+      if (d2 == d3) {
+        return "equilateral";
+      } else {
+        return "isosceles";
+      }
+    } else {
+      if (d2 == d3) {
+        return "isosceles";
+      } else {
+        if (d1 == d3) {
+          return "isosceles";
+        }
+      }
+    }
+    return "scalene";
+  }
 }
