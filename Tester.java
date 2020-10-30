def73aa03a8cf606b4dcffc54cd53897cd655ce6
Jason Zhang <jzhang20@stuy.edu>
2020-10-30 00:41:06 -0400

    hw 13

--- Tester.java
+++ Tester.java
@@ -8,18 +8,23 @@ public class Tester {
     Triangle x = new Triangle (0, 0, 3, 4, 0, 8);
     System.out.println("DISTANCE");
     System.out.println(a.distanceTo(b));
+    System.out.println();
     System.out.println("POINT EQUALS");
     System.out.println(a.equals(b));
     System.out.println(a.equals(d));
+    System.out.println();
     System.out.println("PERIMETERS");
     System.out.println(w.getPerimeter());
     System.out.println(x.getPerimeter());
+    System.out.println();
     System.out.println("AREA");
     System.out.println(w.getArea());
     System.out.println(x.getArea());
+    System.out.println();
     System.out.println("CLASSIFY (THEORETICALLY CONGRUENT TRIANGLE)");
     System.out.println(w.classify());
     System.out.println(x.classify());
+    System.out.println();
     System.out.println("CLASSIFY (GENERAL TRIANGLES)");
     Point g = new Point (0, 0);
     Point h = new Point (3, 4);
@@ -28,5 +33,13 @@ public class Tester {
     Triangle z = new Triangle (g, h, i);
     System.out.println(y.classify());
     System.out.println(z.classify());
+    System.out.println();
+    System.out.println("STRINGIFY");
+    System.out.println(w.toString());
+    System.out.println();
+    System.out.println("MUTATOR TEST");
+    System.out.println("perimeter of triangle w = " + w.getPerimeter());
+    w.setVertex(0, i);
+    System.out.println("perimeter of triangle w = " + w.getPerimeter());
   }
 }
